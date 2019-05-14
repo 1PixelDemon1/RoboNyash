8ccd9c121460c3c46f56301c94ba418ba5e026d9
1PixelDemon1 <46719045+1PixelDemon1@users.noreply.github.com>
2019-05-14 16:11:01 +0500

    1.1
    1.1

---
+++ VIMers/welcome_page.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>
+
+<head>
+    <meta charset = "utf-8">
+    <link rel="stylesheet" href="style.css">
+    <title>Welcome Page</title>
+    
+</head>
+<body>
+    <div class = "header">
+        <img id = "img_logo" src = "images/logo_1.png">
+        <div class = "img_icon"><a class = "icon_login" href = "auth.php"><img src = "images/log-in_1.png" width = "100px"></a></div>    
+        <div class = "panel_header">
+        
+    </div>
+    </div>
+
+</body>
+
+<?php
+
+?>
