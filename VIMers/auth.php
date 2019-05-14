d8d666586b6d4302821492d0196dc53f9158fc24
1PixelDemon1 <46719045+1PixelDemon1@users.noreply.github.com>
2019-05-14 16:11:27 +0500

    1.2

---
+++ VIMers/auth.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<style>
+    body{
+        background-image: url(images/background_1.jpg);
+        
+        
+        
+    }
+    
+</style>
+
+
+<head>
+    <meta charset="utf-8">
+    <title>Авторизация</title>
+    <link href = "style.css" rel = "stylesheet">
+    
+</head>
+<body>
+    <fieldset style = "position: absolute; width:50%; left:25%;top:40%;">
+        
+        <legend align = "center" style = "color:white;">Авторизуйтесь</legend>
+        <form action = "auth_checker.php" method = "post" autocomplete="on">
+            <div style = "position:relative;left:35%;">
+                <input  type = "text" style = "width: 30%; " name = "login" placeholder="Логин">
+                <br>
+                <input  type = "password" style = "width: 30%;" name = "password" placeholder="Пароль">
+                <br>
+                <input type = "submit" value = "Log-In">
+            </div>
+        </form>
+            
+    </fieldset>
+
+    
+    
+</body>
